c1fe7e7e07362849ae2ac6b417179bb8f0b30d67
Kev <kev.chu.pro@gmail.com>
2022-08-15 13:28:43 -0700

    feat: working implementation of basic pascal syntax

---
+++ test_pascal.pas
@@ -0,0 +1,17 @@
+PROGRAM test;
+VAR
+   number     : INTEGER;
+   a, b, c, x : INTEGER;
+   y          : REAL;
+
+BEGIN
+   BEGIN
+      number := 2;
+      a := number;
+      b := 10 * a + 10 * number DIV 4;
+      c := a - - b
+   END;
+   x := 11;
+   y := 20 / 7 + 3.14;
+
+END.
